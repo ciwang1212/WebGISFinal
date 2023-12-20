1ee43ff8e97e7982598af85133b6dcdbedf77881
ciwang1212 <154347448+ciwang1212@users.noreply.github.com>
2023-12-20 20:25:36 +0800

    Add files via upload

---
+++ data/TITIKBOLAtitik_2.js
@@ -0,0 +1,11 @@
+var json_TITIKBOLAtitik_2 = {
+"type": "FeatureCollection",
+"name": "TITIKBOLAtitik_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "NAMA LAPANGAN": "PEMUDA BENTENG", "LOKASI": "Jl. Jenderal Sudirman Kecamatan Benteng", "GAMBAR": "C:\\Users\\Acer\\Pictures\\PaketTraser\\Benteng.jpg" }, "geometry": { "type": "Point", "coordinates": [ 120.45812, -6.1182 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "NAMA LAPANGAN": "TANGKALA", "LOKASI": "Jl Patori Benteng Kecamamatan Bontomanai Desa Tangkala", "GAMBAR": "C:\\Users\\Acer\\Pictures\\PaketTraser\\Tankala.jpg" }, "geometry": { "type": "Point", "coordinates": [ 120.452866, -6.043736 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "NAMA LAPANGAN": "BARUGAIYA", "LOKASI": "Jl Patori Benteng Kecamamatan Bontomanai Desa Barugaia", "GAMBAR": "C:\\Users\\Acer\\Pictures\\PaketTraser\\Barugaiya.jpg" }, "geometry": { "type": "Point", "coordinates": [ 120.44993, -6.03971 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "NAMA LAPANGAN": "BUKI", "LOKASI": "Jl Pahlawan Kecamatan Buki Desa Buki", "GAMBAR": "C:\\Users\\Acer\\Pictures\\PaketTraser\\Buki.jpg" }, "geometry": { "type": "Point", "coordinates": [ 120.449985, -5.971886 ] } }
+]
+}
